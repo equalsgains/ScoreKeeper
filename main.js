082db05ade60d4438f66866ec1ecf0732f63689c
Luis Quintanilla <equalsgains@gmail.com>
2016-12-27 18:13:20 -0700

    added a reset functionality

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 //buttons
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
 //player scores
 var p1Score = 0;
 var p2Score = 0;
@@ -12,13 +13,19 @@ var p2Display = document.querySelector("#p2Display");
 var gameOver = false;
 var winningScore = 5;
 
+//add event listener with call back function
 p1Button.addEventListener("click", function(){
+  // function checks if the game is not over
   if (!gameOver) {
+    //if it's not over, it will add to the player's score
     p1Score++;
+    //if the player score  matches the winningscore
     if (p1Score === winningScore) {
+      p1Display.classList.add("winner");
       console.log("Game Over!");
       gameOver = true;
     }
+    //then stop the code right here.
     p1Display.textContent = p1Score;
   }
 
@@ -28,10 +35,21 @@ p2Button.addEventListener("click", function(){
   if (!gameOver) {
     p2Score++;
     if (p2Score === winningScore) {
+      p2Display.classList.add("winner");
       console.log("Game Over!");
       gameOver = true;
     }
     p2Display.textContent = p2Score;
   }
 
+});
+
+resetButton.addEventListener("click", function(){
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
 });
