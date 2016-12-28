af62287d70567f25ff514938b28ea204fac12e73
Luis Quintanilla <equalsgains@gmail.com>
2016-12-27 17:14:21 -0700

    added basic functionality with JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("connected");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Score = 0;
+var p1Display = document.querySelector("#p1Display");
+
+p1Button.addEventListener("click", function(){
+  p1Score++;
+  console.log(p1Score);
+  p1Display.textContent = p1Score;
+});
